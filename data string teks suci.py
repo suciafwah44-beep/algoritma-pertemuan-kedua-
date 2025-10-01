98eca1aec6f8cdf2bc23445319389aca83a783d1
suciafwah44-beep <suciafwah44@gmail.com>
2025-10-01 12:01:58 +0700

    Add files via upload

---
+++ data string teks suci.py
@@ -0,0 +1,13 @@
+nama_depan = "Suci"
+nama_belakang = 'Afwah'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '19'
+alamat = 'Bangka kota'
+kata_mutiara = "Perbaiki hari ini untuk hari esok yang lebih baik"
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
